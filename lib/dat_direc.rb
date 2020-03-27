1d838cfbc1e44f7b92b295f28f54683154282aed
Telyn Z <175827+telyn@users.noreply.github.com>
2020-03-27 22:10:27 +0000

    define DatDirec module in dat_direc.rb

--- lib/dat_direc.rb
+++ lib/dat_direc.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+module DatDirec
+end
 require "dat_direc/core/database"
 require "dat_direc/core/column"
 require "dat_direc/core/index"
 require "dat_direc/core/table"
-require "dat_direc/migration_generators"
+#require "dat_direc/migration_generators"
 require "dat_direc/dump_parsers"
