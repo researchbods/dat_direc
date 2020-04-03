29dbcecff56524e698cd44c4fb0e05cd79dbe8bf
Telyn Z <175827+telyn@users.noreply.github.com>
2020-04-03 17:14:15 +0100

    add some placeholder documentation files for Migrations and MigrationGenerators. The latter will need a registration system, same as DumpParsers and Differs, but we'll get to that when we get to that

---
+++ lib/dat_direc/migration_generators.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module DatDirec
+  # module containing the migration generators. These are the code generators
+  # which realise abstract Migration objects into code that can be run as a
+  # migration (e.g an ActiveRecord::Migration subclass)
+  # Haven't designed the API for them yet but I think it'll look something like
+  # this:
+  #
+  # #initialize must support being called with no arguments
+  # #generate_up takes a Migration object and returns a string containing just
+  #  the code one would need to add to an 'up' section of a
+  #  migration without indentation. It is recommended that this job is
+  #  dispatched to other classes designed for the purpose of each migration
+  #  type, in order to keep complexity down.
+  #  (See the ActiveRecordMigration class for an example of this)
+  # #generate_file takes a directory name and an array of migrations and writes
+  #                a complete migration file for them, with a correct filename.
+  #                Note that generating a correct class name (for example) is
+  #                up to the MigrationGenerator itself - DatDirecMigration
+  #                followed by a unique number (a timestamp for example) would
+  #                be fine.
+  module MigrationGenerators
+  end
+end
---
+++ lib/dat_direc/migrations.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module DatDirec
+  # Container module for the abstract migrations DatDirec supports.
+  # After 1.0, Migrations should only be added or removed from this module when
+  # new major versions are released in order to ensure custom
+  # MigrationGenerators do not break
+
+  # There is no standard API for a migration nor any registration mechanism as
+  # it is expected that MigrationsGenerators and ReconciliationStrategies know
+  # how to create and introspect a Migration, respectively.
+  #
+  # Migrations are essentially expected to be value objects with little in the
+  # way of behaviour. Structs would probably be an ideal way to make them. I'm
+  # not 100% on this yet since I haven't (at time of writing) written any
+  # ReconciliationStrategies, Migrations, or MigrationGenerators
+  module Migrations
+  end
+end
